e09e743062e25c278b1af3439dde6e505d25c979
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-02-28 19:25:57 +0100

    feat(project) List users in component

--- src/app/layout/users/component/users.component.ts
+++ src/app/layout/users/component/users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IUser } from '../shared/user';
+import { IUser, ROLES } from '../shared/user';
 import { NgRedux, select } from '@angular-redux/store';
 import { Observable } from 'rxjs/Observable';
 import { IAppState } from '../../../shared/store/store.module';
@@ -10,17 +10,22 @@ import { IAppState } from '../../../shared/store/store.module';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-    public users: IUser[];
+  public users: IUser[];
+  public roles = ROLES;
+  public isAdmin = false;
 
-    @select(['users', 'users'])
-    private _users$: Observable<any>;
+  @select(['users', 'users'])
+  private _users$: Observable<any>;
 
-  constructor(
-      private _ngRedux: NgRedux<IAppState>
-  ) {
-      this._users$.subscribe((users) => {
-          this.users = users;
+  constructor(private _ngRedux: NgRedux<IAppState>) {
+    this.isAdmin = (localStorage.getItem('role') === 'admin');
+    this._users$.subscribe((users) => {
+      const userId = localStorage.getItem('userid');
+      this.users = users.map((user) => {
+        user.isRoleEditable = (this.isAdmin && userId !== user.id);
+        return user;
       });
+    });
   }
 
   ngOnInit() {
--- src/app/layout/users/shared/user.ts
+++ src/app/layout/users/shared/user.ts
@@ -1,49 +1,53 @@
 export interface IUser {
-    id: number;
-    username: string;
-    fullname: string;
-    email: string;
-    role: string;
-    profile: IUserProfile;
+  id: number;
+  username: string;
+  fullname: string;
+  email: string;
+  role: string;
+  profile: IUserProfile;
+  isRoleEditable: boolean;
 }
 
 export interface IUserProfile {
-    userId: number;
-    first_name: string;
-    last_name: string;
-    picture_url: string;
+  userId: number;
+  first_name: string;
+  last_name: string;
+  picture_url: string;
 }
 
 export class UserProfile implements IUserProfile {
-    public userId: number;
-    public first_name: string;
-    public last_name: string;
-    public picture_url: string;
+  public userId: number;
+  public first_name: string;
+  public last_name: string;
+  public picture_url: string;
 
-    constructor(elem: any) {
-        this.userId = elem.userId;
-        this.first_name = elem.first_name;
-        this.last_name = elem.last_name;
-        this.picture_url = elem.picture_url;
-    }
+  constructor(elem: any) {
+    this.userId = elem.userId;
+    this.first_name = elem.first_name;
+    this.last_name = elem.last_name;
+    this.picture_url = elem.picture_url;
+  }
 }
 
-export class User implements IUser{
-    public id: number;
-    public username: string;
-    public fullname: string;
-    public email: string;
-    public role: string;
-    public profile: UserProfile;
+export class User implements IUser {
+  public id: number;
+  public username: string;
+  public fullname: string;
+  public email: string;
+  public role: string;
+  public profile: UserProfile;
+  public isRoleEditable: boolean;
 
-    constructor(elem: any) {
-        this.id = elem.id;
-        this.username = elem.username;
-        this.fullname = elem.fullname;
-        this.email = elem.email;
-        this.role = elem.role;
-        if (elem.profile) {
-            this.profile = new UserProfile(elem.profile);
-        }
+  constructor(elem: any) {
+    this.id = elem.id;
+    this.username = elem.username;
+    this.fullname = elem.fullname;
+    this.email = elem.email;
+    this.role = elem.role;
+    if (elem.profile) {
+      this.profile = new UserProfile(elem.profile);
     }
+  }
 }
+
+export const ROLES = ['user', 'manager', 'admin'];
