2f4fa3f12c77ec7c9defc84906d168c0562f89a3
Gabriel Munteanu <gabimunteanu.sdt@gmail.com>
2018-03-01 09:25:24 +0100

    feat(project) List users in component

--- src/app/layout/users/shared/users.services.ts
+++ src/app/layout/users/shared/users.services.ts
@@ -7,38 +7,39 @@ import { environment } from '../../../../environments/environment';
 
 @Injectable()
 export class UsersService {
-    public url = environment.apiUrl;
-    public token = environment.apiToken;
-    public httpOptions: any;
+  public url = environment.apiUrl;
+  public token = environment.apiToken;
+  public httpOptions: any;
 
-    constructor(
-        private _apiHttp: HttpClient
-    ) {
-        this.httpOptions = {
-            headers: new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Token': this.token
-            })
-        };
-    }
+  constructor(private _apiHttp: HttpClient) {}
 
-    public getUsers() {
-        return this._apiHttp.get(this.url + 'users', this.httpOptions);
-    }
+  public addAuthorization() {
+    this.token = localStorage.getItem('token');
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    };
+  }
 
-    public getMe() {
-        return this._apiHttp.get(this.url + 'users/me', this.httpOptions);
-    }
+  public getUsers() {
+    return this._apiHttp.get(this.url + 'users', this.addAuthorization());
+  }
 
-    public getUser(id: number) {
-        return this._apiHttp.get(this.url + 'users/' + id.toString(), this.httpOptions);
-    }
+  public getMe() {
+    return this._apiHttp.get(this.url + 'users/me', this.addAuthorization());
+  }
 
-    public updateUser(user: IUser) {
-        return this._apiHttp.post(this.url + 'users/' + user.id.toString(), user, this.httpOptions);
-    }
+  public getUser(id: number) {
+    return this._apiHttp.get(this.url + 'users/' + id.toString(), this.addAuthorization());
+  }
 
-    public deleteUser(id: number) {
-        return this._apiHttp.delete(this.url + 'users/' + id.toString(), this.httpOptions);
-    }
+  public updateUser(user: IUser) {
+    return this._apiHttp.post(this.url + 'users/' + user.id.toString(), user, this.addAuthorization());
+  }
+
+  public deleteUser(id: number) {
+    return this._apiHttp.delete(this.url + 'users/' + id.toString(), this.addAuthorization());
+  }
 }
